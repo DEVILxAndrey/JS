7e331a2a67179d5fa1d00b022961f4629d961075
AndreyandKiril <antoninakoroleva16@gmail.com>
2022-02-15 15:18:13 +0700

    js вовод массива рандом на экран

--- obeme.js
+++ obeme.js
@@ -1,13 +1,18 @@
 let arr = [];
-const size = 100;
+function create ()
+{
+let size = parseInt(document.getElementById('len').value);
+
 for (let i=0; i<size; i++)
 {
 	arr[i] = Math.floor(Math.random()*10);
 } 
+let div =document.getElementById('massiv')
 for (let i=0; i<arr.length; ++i)
 	{
-	console.log (arr[i]);
+	div.innerHTML += arr[i] + ', ';
 	}
+}
 /*console.log (arr[0]);
 
 console.log (arr[1]);
