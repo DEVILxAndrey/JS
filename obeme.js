fc0006ca6ef9dfeac9e251e8bd57e1f0b9bdf29c
AndreyandKiril <antoninakoroleva16@gmail.com>
2022-02-15 14:38:27 +0700

    js задача 1

--- obeme.js
+++ obeme.js
@@ -1,6 +1,13 @@
-let i = 0;
-while (i < 10)
-{
-	console.log (i);
-	i += 1
-}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+for (let i=0; i<7; ++i)
+	{
+	console.log (arr[i]);
+	}
+/*console.log (arr[0]);
+
+console.log (arr[1]);
+console.log (arr[2]);
+console.log (arr[3]);
+console.log (arr[4]);
+console.log (arr[5]);
+console.log (arr[6]);*/
