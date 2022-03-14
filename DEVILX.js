d345accf65a9d051c46bd68f47610d90b24d5fdd
DEVIL <Andreyxxl7@yandex.ru>
2022-03-14 16:28:39 +0700

    Pedelia7

--- DEVILX.js
+++ DEVILX.js
@@ -1,15 +1,13 @@
-$(document).ready(function() {
-
-   $("#drag1").draggable({revert:true});
 $(document).ready(function(){
 
    $("#drag1").draggable({stack:"#drop1"});
+   $("#drag2").draggable({stack:"#drop1",revert:true});
    $("#drop1").droppable({
+      accept:"#drag1",
       over:function(){
          $("#drag1").css("background-color","#d7fa99");
          $("#drop1").css("background-color","#d7fa99");
-      },
-      drop:function(){
+@@ -10,7 +12,6 @@ $(document).ready(function(){
          $("#drag1").css("display","none");
          $("#drop1").css("background-color","#c4f66f");
          $("#drop1").html("Перемещение завершено успешно.");
@@ -17,4 +15,4 @@ $(document).ready(function(){
       }
    });
 
-}); 
+});
